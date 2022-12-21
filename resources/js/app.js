1cab109f080dc44fe872ad7b0cdb4409e20cbd3a
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-12-21 18:18:55 +0100

    bonus senza stilizzazione

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,28 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+let modalCollection = document.getElementsByClassName("myModal");
+
+let btnCollection = document.getElementsByClassName("myBtn");
+
+let spanColllection = document.getElementsByClassName("close");
+
+for(let i=0; i<btnCollection.length; i++){
+
+    btnCollection[i].onclick = function(e) {
+        e.preventDefault();
+        modalCollection[i].style.display = "block";
+    }
+
+    spanColllection[i].onclick = function() {
+        modalCollection[i].style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == modalCollection) {
+        for(let element of modalCollection){
+            element.style.display = "none";
+        }
+    }
+}
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -15,6 +15,7 @@ $linksImage = config('comicsdb.linksImage');
             </div>
             <div class="cards">
                 @foreach ($comics as $key => $comic)
+                
                 <div class="flip-card">
                     <div class="flip-card-inner">
                         <div class="flip-card-front">
@@ -26,17 +27,29 @@ $linksImage = config('comicsdb.linksImage');
                         <div class="flip-card-back">
                             <a href="{{route('comics.show', ['comic'=> $comic->id])}}">
                                 <div class="img-box">
-                                    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                                    <button class="delete bg-danger myBtn"><i class="fa-solid fa-trash"></i></button>
+                                    {{-- <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
                                         @csrf
                                         @method('DELETE')
                                         <button class="delete bg-danger" type="submit"><i class="fa-solid fa-trash"></i></button>
-                                    </form>
+                                    </form> --}}
                                     <img src="{{$comic['thumb']}}" alt="">
                                     <a href="{{route('comics.edit',$comic->id)}}" class="edit">Edit</a>
                                 </div>
                             </a>
                             <span class="title">{{$comic['price']}}$</span>
                         </div>
+                    </div>
+                </div>
+                <div class="modal myModal">
+                    <div class="modal-content">
+                        <span class="close">&times;</span>
+                        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <span>sei sicuro di elimiare {{$comic->series}}</span>
+                        <button  type="submit"><i class="fa-solid fa-trash"></i></button>
+                        </form>
                     </div>
                 </div>
                 @endforeach
